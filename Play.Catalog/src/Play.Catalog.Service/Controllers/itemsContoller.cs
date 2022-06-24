e8179015bc9921d0e02996c1b902d746e3f09fec
smehta@mms.org <smehta@mms.org>
2022-06-23 23:21:08 -0400

    added circuit braker pattern to limit resource exhaustion and partially added contracts project for async MassTransit based RabbutMQ queues

--- Play.Catalog/src/Play.Catalog.Service/Controllers/itemsContoller.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/itemsContoller.cs
@@ -23,16 +23,34 @@ namespace Play.Catalog.Service.Controllers
         // };
 
         private readonly IRepository<Item> itemRepo;
+        private static int requestCounter = 0;
 
         public ItemsContoller(IRepository<Item> itemRepository)
         {
             this.itemRepo = itemRepository;
         }
         [HttpGet]
-        public async Task<IEnumerable<ItemDto>> GetAsync()
+        public async Task<ActionResult<ItemDto>> GetAsync()
         {
+            requestCounter++;
+            Console.WriteLine($"Request {requestCounter}: Starting..");
+
+            if (requestCounter <= 2)
+            {
+                Console.WriteLine($"Request {requestCounter}: Delaying...");
+                await Task.Delay(TimeSpan.FromSeconds(10));
+            }
+
+            if (requestCounter <= 4)
+            {
+                Console.WriteLine($"Request {requestCounter}: 500 (Internal Server Error)");
+                return StatusCode(500);
+            }
+
             var items = (await itemRepo.GetAllAsync()).Select(item => item.AsDto());
-            return items;
+
+            Console.WriteLine($"Request {requestCounter}: 200 (ok)");
+            return Ok(items);
         }
 
         [HttpGet("{id}")]
--- Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -12,6 +13,8 @@ using Microsoft.OpenApi.Models;
 using Play.Common.MongoDB;
 using Play.Inventory.Service.Client;
 using Play.Inventory.Service.Entities;
+using Polly;
+using Polly.Timeout;
 
 namespace Play.Inventory.Service
 {
@@ -30,9 +33,41 @@ namespace Play.Inventory.Service
             services.AddMongo()
                     .AddMongoRepository<InventoryItem>("inventoryItems");
 
-            services.AddHttpClient<CatalogClient>(client => {
+            Random jiterer = new Random();
+
+            services.AddHttpClient<CatalogClient>(client =>
+            {
                 client.BaseAddress = new Uri("https://localhost:5001");
-            });
+            })
+            .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+                5,
+                retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                                    + TimeSpan.FromMilliseconds(jiterer.Next(0, 1000)),
+                onRetry: (outcome, timespan, retryAttempt) =>
+                {
+                    var serviceProvider = services.BuildServiceProvider();
+                    serviceProvider.GetService<ILogger<CatalogClient>>()
+                        .LogWarning($"Delay for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+                }
+            ))
+            .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+               3,
+               TimeSpan.FromSeconds(15),
+               onBreak: (outcome, timespan) =>
+               {
+                   var serviceProvider = services.BuildServiceProvider();
+                   serviceProvider.GetService<ILogger<CatalogClient>>()
+                       .LogWarning($"opening the circuit for {timespan.TotalSeconds}");
+               },
+               onReset: () =>
+               {
+                   var serviceProvider = services.BuildServiceProvider();
+                   serviceProvider.GetService<ILogger<CatalogClient>>()
+                       .LogWarning($"closing the circuit....");
+               }
+            ))
+            .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+
 
 
             services.AddControllers();
